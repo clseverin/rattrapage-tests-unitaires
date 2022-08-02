4b7710d24b70823eba26855541090e50d3d1665b
clseverin <clelia.severin@ynov.com>
2022-08-02 13:08:49 +0200

    Update test_Vector.py

--- test_Vector.py
+++ test_Vector.py
@@ -1,2 +1,9 @@
+from torch import true_divide
 import Vector
 
+def test___eq__():
+    v1 = [1,2]
+    v2 = [1,2]
+    expected_result = True
+    actual_result = Vector.__eq__(v1, v2)
+    assert expected_result == actual_result
